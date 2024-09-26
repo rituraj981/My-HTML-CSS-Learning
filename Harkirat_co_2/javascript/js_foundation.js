f3bb7b501d5874b1e41db869f0d62bf94fc35a8f
Rituraj pandey <117599621+rituraj981@users.noreply.github.com>
2024-09-26 04:05:53 +0000

    My HTML-CSS Learnings

---
+++ Harkirat_co_2/javascript/js_foundation.js
@@ -0,0 +1,114 @@
+// let firstname = "Rituraj";
+// let age = 23;
+// let isMarried = false;
+
+// console.log("this persons name is " + firstname + " and sge is " + age);
+
+// console.log("This person name is");
+// console.log(firstname);
+// console.log("and he is");
+// console.log(age);
+
+
+
+// let firstname = "Rituraj";
+// let age = 23;
+// let isMarried = false;
+
+// if(isMarried == true){
+//     console.log(firstname + " is married");
+// } else {
+//     console.log(firstname + " is not married");
+// }
+
+
+// or
+
+
+// let firstname = "Rituraj";
+// let age = 23;
+// let isMarried = true;
+
+// if(isMarried == true){
+//     console.log(firstname + " is married");
+// } 
+// if(isMarried == false){
+//     console.log(firstname + " is not married");
+// }
+
+
+
+// let answer = 100;
+
+// for (let i = 0; i <= 100; i++) {
+//     answer = answer + i;
+// }
+// console.log(answer)
+
+
+// ******************************** Questions and their answers ********************************
+
+// 1. Write a program to greet a person given their first and last name
+
+// let firstname = "Rituraj";
+// let lastname = "Pandey";
+
+// console.log("hey!! "+ firstname +" "+ lastname + " How are you?")
+
+
+
+// 2. Write a program that greets based on their gender(if else)
+
+
+// let gender = "male";
+
+// if(gender === "male"){
+//     console.log("hello sir!! How are you?");
+// }
+// else if(gender === "female"){
+//     console.log("hello ma'am!! How are you?");
+// }
+// else{
+//     console.log("hey!! How are you?");
+// }
+
+
+
+// 3. Write a program that counts and add 0-1000 and prints(for loop)
+
+// let number = 1000;
+// let answer = 0;
+
+// for(i=0; i<=1000; i++){
+//     answer = answer + i;
+// }
+// console.log(answer);
+
+
+
+
+
+
+
+
+
+
+
+// 1. array
+// 2. Objects 
+
+
+
+// 1. Write a program to print all the biggest number in an array.
+
+
+
+// 2. Write a program to print the biggest number in an array.
+
+
+
+// 3. Write a program that prints all the male people's first name given a complex object.
+
+
+
+// 4. Write a program that reverses all the elements of an array.
---
+++ Harkirat_co_2/javascript/tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+let gender = "male";
+
+if(gender === "male"){
+    console.log("hello sir!! How are you?");
+}
+else if(gender === "female"){
+    console.log("hello ma'am!! How are you?");
+}
+else{
+    console.log("hey!! How are you?");
+}
